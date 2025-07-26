2514ba6c51127629d286769c2e75858730ad78b6
iker <velezlucasiker1@gmail.com>
2025-07-26 00:57:47 -0500

    Enhanced UI with complementary color palette

--- weather_app/src/index.js
+++ weather_app/src/index.js
@@ -3,17 +3,16 @@ import "./styles.css";
 
 const key = "A4G94C3GM9CKR6XA64CHYMMRQ";
 const url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
-let city = "ciudad del cabo";
 
 let searchCity = document.getElementById("input");
 let search = document.getElementById("search"); 
+
+let city = "ciudad del cabo";
 
 search.addEventListener("click", (event) => {
     event.preventDefault();
 
     city = searchCity.value;
-    console.log(city);
-
     getWeather();
 })
 
@@ -39,18 +38,22 @@ const getWeather = async function () {
     } catch  (error) {
      console.log(error);
      console.log("something went wrong");
+     handleError();
     }
 }
 
 let weatherCard = document.getElementById("weather-card");
 
+const handleError = async function () {
+    alert("That city does not exist");
+}
+
 const domdAttachment = async function () {
-        
     weatherCard.children[0].innerHTML = report.condition;
     weatherCard.children[1].innerHTML = report.city;
-    weatherCard.children[2].innerHTML = report.temp;
-    weatherCard.children[3].innerHTML = report.feelslike;
-    weatherCard.children[4].innerHTML = report.humidity;
+    weatherCard.children[2].innerHTML = report.temp + "&#8457";
+    weatherCard.children[3].innerHTML = "Feels like" + report.feelslike + "&#8457";
+    weatherCard.children[4].innerHTML = "Humidity" + report.humidity + "%";
 }
 
 getWeather();
