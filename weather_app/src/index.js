e11b1761088ed8a261d6886ebab6cda82c085b4c
iker <velezlucasiker1@gmail.com>
2025-07-25 21:58:34 -0500

    Display weather info in the .card

--- weather_app/src/index.js
+++ weather_app/src/index.js
@@ -17,10 +17,40 @@ search.addEventListener("click", (event) => {
     getWeather();
 })
 
+let report = {};
+
 const getWeather = async function () {
-    let petition = await fetch( url + city + "?key=" + key, {mode: "cors"})
-    let result = await petition.json();
-    console.log(result.days[0].temp, result.days[1].temp, result.days[2].temp, result.days[3].temp, result.days[4].temp);
+
+    try {
+     let petition = await fetch( url + city + "?key=" + key, {mode: "cors"})
+     let result = await petition.json();
+
+     report = {
+        city: result.resolvedAddress,
+        condition: result.currentConditions.conditions,
+        temp: result.currentConditions.temp,
+        feelslike: result.currentConditions.feelslike,
+        humidity: result.currentConditions.humidity,
+     }
+
+     await report;
+     domdAttachment();
+
+    } catch  (error) {
+     console.log(error);
+     console.log("something went wrong");
+    }
+}
+
+let weatherCard = document.getElementById("weather-card");
+
+const domdAttachment = async function () {
+        
+    weatherCard.children[0].innerHTML = report.condition;
+    weatherCard.children[1].innerHTML = report.city;
+    weatherCard.children[2].innerHTML = report.temp;
+    weatherCard.children[3].innerHTML = report.feelslike;
+    weatherCard.children[4].innerHTML = report.humidity;
 }
 
 getWeather();
